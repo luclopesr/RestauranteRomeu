c827fb4cbc9078312990386eceb725047f2cb3ac
Gabriel da Silva Rosa <65716283+GabrielRosa09@users.noreply.github.com>
2021-10-25 21:47:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# importações das bibliotecas flask 
 from flask import Flask, render_template
 from flask.helpers import url_for
 from flask_wtf import FlaskForm
@@ -8,11 +9,13 @@ from wtforms.validators import DataRequired, EmailField
 from wtforms.fields.html5 import DateField
 from flask_sqlalchemy import SQLAlchemy
 
+# início da aplicação e definição da rota do banco de dados
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/reserva'
 app.config['SECRET_KEY'] = 'Arrozbom'
 
+# criação do banco de dados
 db = SQLAlchemy(app)
 
 class Reserva(db.Model):
@@ -23,6 +26,7 @@ class Reserva(db.Model):
     qtd_pessoas = db.Column(db.Integer)
     mesa = db.Column(db.String(7))
 
+# criação do formulário
 class ReservaForm(FlaskForm):
     name = StringField("Nome: ", validators=[DataRequired()])
     email = EmailField('Email: ', [validators.DataRequired(), validators.Email()])
@@ -32,16 +36,26 @@ class ReservaForm(FlaskForm):
     mesa = SelectField("N° da Mesa: ", choices=[('Mesa 1'), ('Mesa 2'), ('Mesa 3'), ('Mesa 4'), ('Mesa 5'), ('Mesa 6'), ('Mesa 7'), ('Mesa 8'), ('Mesa 9'), ('Mesa 10'), ('Mesa 11')], validators=[DataRequired()])
     submit = SubmitField("ENVIAR")
 
+# início das rotas do site
 @app.route('/name', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def cadastro():
     form = ReservaForm()
-
+   
+    # validação quando o usuário apertar o botão de "ENVIAR" 
     if form.validate_on_submit():
+        
+        # validação do nome
+        name_validate = form.name.data
+        if any(chr.isdigit() for chr in name_validate) == True:
+             return render_template("erroalertName.html", form = form)
+        
+        # validação do dia, mês e horário
         validar = Reserva.query.filter_by(data = form.data.data).first()
         if validar:
             if (validar.horario == form.horario.data ) and (validar.mesa == form.mesa.data):
                 return render_template("erroalert.html", form = form)
+        # aqui os dados estão sendo passados do formulário para o banco de dados 
         new_reserva = Reserva(name=form.name.data, email=form.email.data, data=form.data.data, horario=form.horario.data, qtd_pessoas=form.qtd_pessoas.data, mesa=form.mesa.data)
         db.session.add(new_reserva)
         db.session.commit()
